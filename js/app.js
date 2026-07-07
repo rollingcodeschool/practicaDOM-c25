c0b4fcec66e8894f28b95360b11d9f37dc6593f4
Emilse Arias <emilsearias@gmail.com>
2026-07-07 10:21:49 -0300

    agregamos el mostrar y ocuptar el parrafo en la funcion ocultarParrafo

--- js/app.js
+++ js/app.js
@@ -12,5 +12,16 @@ function cambiarTexto(){
 
 const ocultarParrafo = ()=>{
     const parrafoOculto =  document.getElementById('parrafo-oculto')
-    parrafoOculto.classList.add('d-none')
+    const botonOcultar = document.querySelectorAll('button')
+
+    if(botonOcultar[3].textContent === 'Ocultar'){
+        parrafoOculto.classList.add('d-none')
+        // parrafoOculto.className = 'd-none'
+        botonOcultar[3].textContent = 'Mostrar'
+        botonOcultar[3].classList.replace('btn-danger','btn-success')
+    }else{
+        parrafoOculto.classList.remove('d-none')
+        botonOcultar[3].textContent = 'Ocultar'
+        botonOcultar[3].classList.replace('btn-success','btn-danger')
+    }
 }
